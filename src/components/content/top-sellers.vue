117fd35a5a06a9f64e4a3adbc12616a11ff9b91b
Andrei O <andrei@flashsoft.eu>
2022-06-09 00:53:21 +0300

    add: export data to top sellers page & added headers to csv

--- src/components/content/top-sellers.vue
+++ src/components/content/top-sellers.vue
@@ -17,16 +17,25 @@
 
       <o-table-column v-slot="props" field="account" label="USER">
         <div v-if="props.row.account === 'unknown'" class="inline">
-        <UserIcon :key="iconsColor" :color="iconsColor" />
+          <UserIcon :key="iconsColor" :color="iconsColor" />
           {{ props.row.account }}
         </div>
         <div v-else class="inline">
-        <a v-if="!props.row.account.includes(',')" :href="`https://app.yup.io/${props.row.account}`" rel="nofollow" target="_blank">
-          <UserIcon :key="iconsColor" :color="iconsColor" />
-          {{ props.row.account }}</a>
-        <a v-for="acc of props.row.account.split(',')" v-else :key="acc" :href="`https://app.yup.io/${acc}`" rel="nofollow" target="_blank">
-          <UserIcon :key="iconsColor" :color="iconsColor" />
-          {{ acc }}</a>
+          <a v-if="!props.row.account.includes(',')" :href="`https://app.yup.io/${props.row.account}`" rel="nofollow" target="_blank">
+            <UserIcon :key="iconsColor" :color="iconsColor" />
+            {{ props.row.account }}</a
+          >
+          <a
+            v-for="acc of props.row.account.split(',')"
+            v-else
+            :key="acc"
+            :href="`https://app.yup.io/${acc}`"
+            rel="nofollow"
+            target="_blank"
+          >
+            <UserIcon :key="iconsColor" :color="iconsColor" />
+            {{ acc }}</a
+          >
         </div>
       </o-table-column>
 
@@ -45,9 +54,14 @@
       </o-table-column>
 
       <o-table-column v-slot="props" field="relatedAcc" label="Related Accounts">
-        <o-tooltip v-if="String(props.row.related_accounts_full).length > 40"  :triggers="['click']" :autoClose="['outside', 'escape']" :multiline="true">
+        <o-tooltip
+          v-if="String(props.row.related_accounts_full).length > 40"
+          :triggers="['click']"
+          :autoClose="['outside', 'escape']"
+          :multiline="true"
+        >
           <template #content>
-            <div class="inline" style=" font-size: 0.85rem">
+            <div class="inline" style="font-size: 0.85rem">
               {{ props.row.related_accounts_full }}
             </div>
           </template>
@@ -76,6 +90,28 @@
         <o-button :class="`btn`" @click="curPage + 1 > 5 ? null : setCurentPage(curPage + 1)">⏵</o-button>
       </router-link>
     </div>
+    <div class="mt-2">
+      <button
+        type="button"
+        class="acbtn inline-flex items-center rounded-lg bg-yellow-500 hover:bg-pink-500 px-4 py-2 text-white m-2"
+        :disabled="btnLoaders.exportJson"
+        @click="dwJson()"
+      >
+        <BtnSpinner v-if="btnLoaders.exportJson" />
+        <FileDownloadIcon class="m-1" />
+        <span class="font-medium font-bold">Download JSON for this period for top 100 users</span>
+      </button>
+      <button
+        type="button"
+        class="acbtn inline-flex items-center rounded-lg bg-yellow-500 hover:bg-pink-500 hover:to-yellow-500 px-4 py-2 text-white m-2"
+        :disabled="btnLoaders.exportCSV"
+        @click="dwCSV()"
+      >
+        <BtnSpinner v-if="btnLoaders.exportCSV" />
+        <FileDownloadIcon class="m-1" />
+        <span class="font-medium font-bold">Download CSV for this period for top 100 users</span>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -85,7 +121,9 @@ import DangLoader from '@/components/content/vote-list/loader.vue'
 import UserIcon from '@/components/content/icons/user.vue'
 import TokenIcon from '@/components/content/icons/tokenYup.vue'
 import ETHIcon from '@/components/content/icons/eth.vue'
+import FileDownloadIcon from '@/components/content/icons/fileDownload.vue'
 import { useMainStore } from '@/store/main'
+import { exportFile, convertToCSV } from '@/utils'
 
 import {
   onMounted,
@@ -100,7 +138,7 @@ import {
 
 export default defineComponent({
   name: 'TopSellers',
-  components: { DangLoader, UserIcon, ETHIcon, TokenIcon },
+  components: { DangLoader, UserIcon, ETHIcon, TokenIcon, FileDownloadIcon },
   props: {
     pageNum: {
       required: true,
@@ -116,6 +154,12 @@ export default defineComponent({
     const iconsColor = ref(store.theme === 'dark' ? '#ccc' : '#020201')
 
     const data: Ref<Record<string, string | number | boolean>[]> = ref([])
+    const allSellers: Ref<Record<string, string | number | boolean>[]> = ref([])
+
+    const btnLoaders = ref({
+      exportJson: false,
+      exportCSV: false
+    })
 
     const setCurentPage = (page: number) => {
       if (page !== curPage.value) {
@@ -138,6 +182,19 @@ export default defineComponent({
       return data
     }
 
+    const prepareSellersObj = (item: Record<string, string | number>) => {
+      let relAcc = item?.related_accounts ?? '[]'
+      relAcc = (relAcc as string).length > 40 ? (relAcc as string).slice(0, 40) + '...' : relAcc
+      return {
+        address: item.address,
+        yup_sold: item.yup_sold,
+        eth_recived: item.eth_recived,
+        account: item.account,
+        related_accounts: relAcc,
+        related_accounts_full: item.related_accounts
+      }
+    }
+
     const getSellers = async (page: number) => {
       if (!page || page < 1) {
         page = 1
@@ -157,17 +214,22 @@ export default defineComponent({
         throw new Error(`Request failed with status ${req.status}`)
       }
 
-      return (await req.json()).data.map((item: Record<string, string | number>) => {
-        let relAcc = item?.related_accounts ?? "[]"
-        relAcc = (relAcc as string).length > 40 ? (relAcc as string).slice(0, 40) + '...' : relAcc
-        return {
-          address: item.address,
-          yup_sold: item.yup_sold,
-          eth_recived: item.eth_recived,
-          account: item.account,
-          related_accounts: relAcc,
-          related_accounts_full: item.related_accounts,
+      return (await req.json()).data.map(prepareSellersObj)
+    }
+
+    const getAllTopSellers = async () => {
+      return new Promise<void>((resolve) => {
+        const reqsPromises = []
+
+        for (let i = 1; i <= 5; i++) {
+          reqsPromises.push(getSellers(i))
         }
+        Promise.all(reqsPromises).then((res) => {
+          allSellers.value = res.reduce((acc, cur) => {
+            return [...acc, ...cur]
+          }, [])
+          resolve()
+        })
       })
     }
 
@@ -186,6 +248,43 @@ export default defineComponent({
       }
     })
 
+    const dwCSV = async () => {
+      btnLoaders.value.exportCSV = true
+      if (allSellers.value.length === 0) {
+        await getAllTopSellers()
+      }
+
+      const withHeader = [{
+        address: 'Address',
+        yup_sold: 'YUP Sold',
+        eth_recived: 'ETH Recived',
+        account: 'Account',
+        related_accounts: 'Related Accounts'
+      }, ...allSellers.value]
+
+      exportFile(`TOP Sellers.csv`, convertToCSV(withHeader.map( v => {
+        v.account = (v.account as string).replace(/,/gm, ' ')
+        v.related_accounts_full = (v.related_accounts_full as string).replace(/,/gm, ' ')
+        return {
+          address: v.address,
+          yup_sold: v.yup_sold,
+          eth_recived: v.eth_recived,
+          account: v.account,
+          related_accounts: v.related_accounts_full
+        }
+      })), 'csv')
+      btnLoaders.value.exportCSV = false
+    }
+
+    const dwJson = async () => {
+      btnLoaders.value.exportJson = true
+      if (allSellers.value.length === 0) {
+        await getAllTopSellers()
+      }
+      exportFile(`TOP Sellers.json`, JSON.stringify(allSellers.value, null, 2), 'json')
+      btnLoaders.value.exportJson = false
+    }
+
     onMounted(async () => {
       getTableData(curPage.value)
     })
@@ -194,7 +293,7 @@ export default defineComponent({
       // do nothing
     })
 
-    return { data, isTableLoading, curPage, setCurentPage, iconsColor }
+    return { data, isTableLoading, curPage, setCurentPage, iconsColor, dwCSV, dwJson, btnLoaders }
   }
 })
 </script>
@@ -209,13 +308,13 @@ export default defineComponent({
     text-align: center;
   }
 
-.o-tip__content {
-  word-break: break-all;
-  min-width: 15rem;
-  background-color: var(--glassBg);
-  color: aliceblue;
-  border-radius: 4px;
-  padding: 0.5rem;
-}
+  .o-tip__content {
+    word-break: break-all;
+    min-width: 15rem;
+    background-color: var(--glassBg);
+    color: aliceblue;
+    border-radius: 4px;
+    padding: 0.5rem;
+  }
 }
 </style>
