f474ff7939801a1e522be512f8d54baab87a537b
Andrei O <andrei@flashsoft.eu>
2023-03-01 20:42:58 +0200

    revert

---
+++ apps/yup-live/src/notice.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="absolute bg-gray-900 text-[2rem] w-full  h-full flex">
+
+<div class="relative shrink bg-gray-800 pt-[10vw] text-gray-100 mx-10 p-8 w-full">
+    Due to my perception that the YUP team does not condone alternative community projects.<br>
+    I stopped maintaining this YUP-live side project.
+    <br><br>
+    Thanks for understanding, and thanks for using this project.
+</div>
+</div>
+</template>
