fc3f13e868651b88d19434e9a1815519ce9850b3
Andrei O <andrei@flashsoft.eu>
2024-05-25 05:05:26 +0300

    chore: re-enable ssg

--- apps/yup-live/src/main.ts
+++ apps/yup-live/src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
+// import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-// import { ViteSSG } from 'vite-ssg'
+import { ViteSSG } from 'vite-ssg'
 import App from '@/app.vue'
-import { routes, router } from '@/router'
-import { createHead } from '@unhead/vue'
+import { routes } from '@/router'
+// import { createHead } from '@unhead/vue'
 
 import '@/assets/twind.css'
 import '@/assets/oruga-patched.scss'
@@ -28,61 +28,63 @@ import { Buffer } from 'buffer'
 globalThis.Buffer = Buffer
 
 
-// export const createApp = ViteSSG(
-//   // the root component
-//   App,
-//   // vue-router options
-//   { routes },
-//   // function to have custom setups
-//   // ({ app, router, routes, isClient, initialState }) => {
-//   async ({ app, initialState }) => {
-//     const pinia = createPinia()
-//     app.use(pinia)
-//     // app.use(createHead())
-//     // app.use(router)
-//     app.use(Dropdown)
-//     app.use(Button)
-//     app.use(Sidebar)
-//     app.use(Tabs)
-//     app.use(Modal)
-//     app.use(Skeleton)
-//     app.use(Switch)
-//     app.use(Radio)
-//     app.use(Table)
-//     app.use(Checkbox)
+export const createApp = ViteSSG(
+  // the root component
+  App,
+  // vue-router options
+  { routes },
+  // function to have custom setups
+  // ({ app, router, routes, isClient, initialState }) => {
+  async ({ app, initialState }) => {
+    const pinia = createPinia()
+    app.use(pinia)
+    // app.use(createHead())
+    // app.use(router)
+    app.use(Dropdown)
+    app.use(Button)
+    app.use(Sidebar)
+    app.use(Tabs)
+    app.use(Modal)
+    app.use(Skeleton)
+    app.use(Switch)
+    app.use(Radio)
+    app.use(Table)
+    app.use(Checkbox)
+    app.use(Input)
+    app.use(Tooltip)
 
 
-//     if (import.meta.env.SSR) {
-//       // this will be stringified and set to window.__INITIAL_STATE__
-//       initialState.pinia = pinia.state.value
-//     } else {
-//       // on the client side, we restore the state
-//       pinia.state.value = initialState?.pinia || {}
-//     }
-//     // router.beforeEach((to, from, next) => {
-//     //     const store = useMainStore(pinia)
+    if (import.meta.env.SSR) {
+      // this will be stringified and set to window.__INITIAL_STATE__
+      initialState.pinia = pinia.state.value
+    } else {
+      // on the client side, we restore the state
+      pinia.state.value = initialState?.pinia || {}
+    }
+    // router.beforeEach((to, from, next) => {
+    //     const store = useMainStore(pinia)
 
-//     //     store.initialize()
-//     //     next()
-//     //   })
-//   }
-// )
+    //     store.initialize()
+    //     next()
+    //   })
+  }
+)
 
-createApp(App)
-  .use(router)
-  .use(Dropdown)
-  .use(Button)
-  .use(Sidebar)
-  .use(Tabs)
-  .use(Modal)
-  .use(Skeleton)
-  .use(Switch)
-  .use(Radio)
-  .use(Table)
-  .use(Tooltip)
-  .use(Checkbox)
-  .use(Input)
-  .use(createPinia())
-  .use(createHead())
-  .provide('routes', routes)
-  .mount('#app')
+// createApp(App)
+//   .use(router)
+//   .use(Dropdown)
+//   .use(Button)
+//   .use(Sidebar)
+//   .use(Tabs)
+//   .use(Modal)
+//   .use(Skeleton)
+//   .use(Switch)
+//   .use(Radio)
+//   .use(Table)
+//   .use(Tooltip)
+//   .use(Checkbox)
+//   .use(Input)
+//   .use(createPinia())
+//   .use(createHead())
+//   .provide('routes', routes)
+//   .mount('#app')
